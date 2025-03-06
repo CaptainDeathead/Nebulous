157e1054b32bf11e26244e209040af93b15a63b9
Joshua Hall <unstableplazma@gmail.com>
2025-03-06 17:18:45 +0800

    Basic splitscreen management

--- Games/Snither/main.py
+++ Games/Snither/main.py
@@ -59,13 +59,64 @@ class Snake:
     def face_down(self) -> None: self.direction = DIRECTION.DOWN
     def face_left(self) -> None: self.direction = DIRECTION.LEFT
 
+class Screen(pg.Surface):
+    def __init__(self, rect: pg.Rect, flags: int = 0) -> None:
+        self.positioning_rect = rect
+
+        super().__init__(rect.size, flags)
+
+    @property
+    def x(self) -> int: return self.positioning_rect.x
+
+    @property
+    def y(self) -> int: return self.positioning_rect.y
+
+    @property
+    def pos(self) -> Sequence[int]: return (self.positioning_rect.x, self.positioning_rect.y)
+
 class Snither:
     PYGAME_INFO: any = pg.display.Info()
     WIDTH: int = PYGAME_INFO.current_w
     HEIGHT: int = PYGAME_INFO.current_h
 
-    def __init__(self) -> None:
-        self.screen = pg.display.set_mode((self.WIDTH, self.HEIGHT), pg.FULLSCREEN | pg.DOUBLEBUFF | pg.HWSURFACE)
+    def __init__(self, num_splitscreen_players: int = 1) -> None:
+        self.num_screens = num_splitscreen_players
+
+        self.display_surf = pg.display.set_mode((self.WIDTH, self.HEIGHT), pg.FULLSCREEN | pg.DOUBLEBUFF | pg.HWSURFACE)
+        self.screens = self.setup_screens()
+        
         self.clock = pg.time.Clock()
 
-        
+    def setup_screens(self) -> List[Screen]:
+        def three_screens() -> List[Screen]:
+            screen_top_left_rect = pg.Rect(0, 0, self.WIDTH // 2, self.HEIGHT // 2)
+            screen_top_right_rect = pg.Rect(self.WIDTH // 2, 0, self.WIDTH // 2, self.HEIGHT // 2)
+            screen_bottom_left_rect = pg.Rect(0, self.HEIGHT // 2, self.WIDTH // 2, self.HEIGHT // 2)
+
+            screen_top_left = Screen(screen_top_left_rect, pg.SRCALPHA)
+            screen_top_right = Screen(screen_top_right_rect, pg.SRCALPHA)
+            screen_bottom_left = Screen(screen_bottom_left_rect, pg.SRCALPHA)
+
+            return [screen_top_left, screen_top_right, screen_bottom_left]
+
+        match self.num_screens:
+            case 1: return [Screen(pg.Rect(0, 0, self.WIDTH, self.HEIGHT), pg.SRCALPHA)]
+            case 2:
+                screen_left_rect = pg.Rect(0, 0, self.WIDTH // 2, self.HEIGHT)
+                screen_right_rect = pg.Rect(self.WIDTH // 2, 0, self.WIDTH // 2, self.HEIGHT)
+
+                screen_left = Screen(screen_left_rect, pg.SRCALPHA)
+                screen_right = Screen(screen_right_rect, pg.SRCALPHA)
+
+                return [screen_left, screen_right] 
+
+            case 3: return three_screens()
+            case 4:
+                screens = three_screens()
+
+                screen_bottom_right_rect = pg.Rect(self.WIDTH // 2, self.HEIGHT // 2, self.WIDTH // 2, self.HEIGHT // 2)
+                screen_bottom_right = Screen(screen_bottom_right_rect, pg.SRCALPHA)
+
+                screens.append(screen_bottom_right)
+
+                return screens
