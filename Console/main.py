5c33738dd78342fae4018ce5c73240ed3a8ee8bf
Joshua Hall <unstableplazma@gmail.com>
2025-04-06 16:18:24 +0800

    Pong; Game crash screen; Extra console info

--- Console/main.py
+++ Console/main.py
@@ -10,6 +10,7 @@ from console_io import IOManager
 from time import sleep, time
 from threading import Thread
 from sys import argv
+from traceback import format_exc
 
 logging.basicConfig()
 logging.root.setLevel(logging.DEBUG)
@@ -18,23 +19,46 @@ logging.basicConfig(level=logging.DEBUG)
 FONTS_PATH = "./UI/Fonts"
 
 class Console:
+    """
+    FIRMWARE FOR NEBULOUS CONSOLE
+    """
+
+    CONSOLE_NAME = "Nebulous REV. A - PROTO"
+    TARGET_FIRMWARE_INFO = {
+        "CONSOLE_NAME": CONSOLE_NAME,
+        "HOST_TYPE": "Linux - Raspberry Pi OS DESKTOP",
+        "FIRMWARE_VERSION": "1.1.0",
+        "REQUIREMENTS": ["Restart console to load new cartridge (auto detection causing controller lag)"]
+    }
+
+    # Allows the console to write data to the cartridge, and verify it is the same to check if cartridge is still inserterd
+    # WARNING: THIS CAN CAUSE INTERUPTIONS TO THE CONTROLLER INPUTS (DELAY + GHOSTING)
+    # WARNING: THIS CAN HAVE INCONSISTENT BEHAVIOUR WHEN PAIRED WITH CONTROLLERS (RANDOM FAILED READS / WRITES)
+    CARTRIDGE_READ_CHECK = False
+
     NUM_CONTROLLER_PORTS = 4
-    TARGET_FPS = 60
-    TESTING = False
+    TARGET_FPS = 60 # Only affects menu
+    TESTING = False # Allows testing in a non-console environment
 
     def __init__(self, testing: bool = False) -> None:
         """
         Steps to initialize console:
-        1. Initialize controllers and controller polling
-        2. Initialize display and screen
-        3. Load into blank console interface
-        4. Initialize cartridge loading
+        1. Initialize console input / output devices (io)
+        2. Initialize controllers and controller polling
+        3. Initialize display and screen
+        4. Load into blank console interface
+        5. Initialize cartridge loading
         """
 
         self.init = False
         self.TESTING = testing
 
-        logging.info("Welcome from the console!!!")
+        logging.info("Welcome from the console!!!\n")
+
+        for key, value in self.TARGET_FIRMWARE_INFO.items():
+            logging.info(f"{key}: {value}")
+
+        print()
 
         self.init_io()
         self.init_controllers()
@@ -42,9 +66,11 @@ class Console:
         self.init_menu_interface()
         self.init_cartridges()
 
+        # All threads will read this every time they update and safely shutdown if it is True.
+        # Used to shutdown console safely.
         self.kill_threads = False
 
-        self.update_controllers = False
+        self.update_controllers = False # Allows for cartridge reader to "read check" its status 'safely'.
         self.controller_check_thread = Thread(target=self.check_controllers)
         self.controller_check_thread.start()
 
@@ -55,7 +81,6 @@ class Console:
         self.init = True
         logging.info("Console startup complete!")
 
-        # TODO: THIS IS JUST FOR TESTING
         if self.TESTING: self.load_cartidge()
 
         self.main()
@@ -71,7 +96,7 @@ class Console:
         self.cartridge_loader.unload_cartridge()
 
         logging.info("Executing 'Nebulous:/Console$ sudo shutdown now'...")
-        os.system("sudo shutdown now")
+        os.system("sudo shutdown now") # WARNING: Requires 'sudo' to not have a password
         exit(0)
 
     def init_io(self) -> None:
@@ -142,6 +167,10 @@ class Console:
                 logging.error("Failed to re-init cartridge loader! Waiting 1 second before retrying...")
                 sleep(1)
 
+        if not self.CARTRIDGE_READ_CHECK:
+            logging.warning("Cartridge read checking disabled! Killing cartridge check thread...")
+            return
+
         while not self.kill_threads:
             if time() - self.last_cartridge_update_check > 2:
                 self.last_cartridge_update_check = time()
@@ -169,6 +198,28 @@ class Console:
     def on_cartridge_remove(self) -> None:
         logging.warning(f"Cartridge no longer reading / writing (disconnected)!")
 
+    def on_game_crash(self) -> None:
+        self.screen.fill((255, 0, 0))
+        
+        title_font = pg.font.Font(f"{FONTS_PATH}/PressStart2P.ttf", 50)
+        self.screen.blit(title_font.render("GAME CRASH!", True, (255, 255, 255)), (100, 100))
+
+        instructions_font = pg.font.Font(f"{FONTS_PATH}/PressStart2P.ttf", 30)
+        self.screen.blit(instructions_font.render("Restart the console using the front IO switch to safely recover.", True, (0, 255, 255), wraplength=self.WIDTH - 100), (100, 200))
+
+        error_font = pg.font.Font(f"{FONTS_PATH}/PressStart2P.ttf", 20)
+        self.screen.blit(error_font.render(f"Error:\n{format_exc()}", True, (0, 0, 0), wraplength=self.WIDTH - 200), (100, 300))
+
+        pg.display.flip()
+        
+        while 1:
+            self.clock.tick(self.TARGET_FPS)
+
+            for event in pg.event.get():
+                ...
+
+            self.update()
+
     def main(self) -> None:
         while not self.cartridge_loaded:
             self.delta_time = self.clock.tick(self.TARGET_FPS)
@@ -177,6 +228,8 @@ class Console:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pass
+
+            self.update()
 
             self.enter_cartridge_rect = self.enter_cartridge_rect.move(self.enter_cartridge_vel[0], self.enter_cartridge_vel[1])
 
@@ -222,13 +275,22 @@ class Console:
         self.cartridge_loaded = True
         self.update_controllers = True
 
-        ConsoleEntry(self.screen, self.update, self.controller_manager.get_num_players, self.controller_manager.controllers)
+        try:
+            ConsoleEntry(self.screen, self.update, self.controller_manager.get_num_players, self.controller_manager.controllers)
+        except Exception as e:
+            logging.error(f"Game crashed with error: {e}!")
 
-        self.main()
+            logging.error(format_exc())
+            
+            # If the user cannot recover by unplugging cartridge then show crash screen
+            if not self.CARTRIDGE_READ_CHECK:
+                self.on_game_crash()
+            else:
+                self.main()
 
 if __name__ == "__main__":
     # Where it all begins...
-    testing = False # DO NOT CHANGE THIS VALUE, HAMISH MATTHEW BRIGGS!!!! USE --test WHEN RUNNING LE SCRIPT FOR TESTING
+    testing = False # DO NOT CHANGE THIS VALUE!!!!! USE --test WHEN RUNNING THE SCRIPT FOR TESTING
 
     if len(argv) > 1:
         if argv[1] == "--test":
